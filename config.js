77ce35f7609879ab358aaa07fefd76a20e6b1ada
JiviteshJamuda <jivitesh2006@gmail.com>
2020-09-03 16:48:50 +0530

    updated commit

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyASyOjOtJla-X-b3io2eLoaUc_bIRFSIIc",
-    authDomain: "book-santa-app.firebaseapp.com",
-    databaseURL: "https://book-santa-app.firebaseio.com",
-    projectId: "book-santa-app",
-    storageBucket: "book-santa-app.appspot.com",
-    messagingSenderId: "69634746716",
-    appId: "1:69634746716:web:6fbbfc110fb4475365f999",
-    measurementId: "G-DLB7XC0JPL"
+  apiKey: "AIzaSyCS_0v42xB8rMEOYY1I-8nkYUg6Y5-g4is",
+  authDomain: "booksanta-bfe61.firebaseapp.com",
+  databaseURL: "https://booksanta-bfe61.firebaseio.com",
+  projectId: "booksanta-bfe61",
+  storageBucket: "booksanta-bfe61.appspot.com",
+  messagingSenderId: "426084860044",
+  appId: "1:426084860044:web:550e522bd3d002baffbba9",
+  measurementId: "G-EEFJZ7WXMX"
   };
 
 
--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, FlatList,Text } from 'react-native';
 import { ListItem, Icon } from 'react-native-elements';
 import firebase from 'firebase';
 import MyHeader from '../components/MyHeader';
-
+import SwipeableFlatlist from "../components/SwipeableFlatlist";
 import db from '../config';
 
 export default class NotificationScreen extends Component{
@@ -14,7 +14,6 @@ export default class NotificationScreen extends Component{
       userId :  firebase.auth().currentUser.email,
       allNotifications : []
     };
-
     this.notificationRef = null
   }
 
@@ -74,10 +73,8 @@ export default class NotificationScreen extends Component{
               </View>
             )
             :(
-              <FlatList
-                keyExtractor={this.keyExtractor}
-                data={this.state.allNotifications}
-                renderItem={this.renderItem}
+              <SwipeableFlatlist
+                allNotifications = {this.state.allNotifications}
               />
             )
           }
